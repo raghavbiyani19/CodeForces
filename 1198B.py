3efd8963d5bff658806744c7e85c0a698247ce01
Raghav Biyani <45257114+raghavbiyani19@users.noreply.github.com>
2020-06-18 01:02:08 +0530

    Welfare State

---
+++ 1198B.py
@@ -0,0 +1,95 @@
+from __future__ import division, print_function
+import os
+import sys
+from io import BytesIO, IOBase
+
+if sys.version_info[0] < 3:
+    from __builtin__ import xrange as range
+    from future_builtins import ascii, filter, hex, map, oct, zip
+def main():
+    n = int(input())
+    arr=list(map(int,input().split()))
+    q = int(input())
+    dict1={}
+    max1=[]
+    for i in range(q):
+        temp=list(map(int,input().split()))
+        if(len(temp)==2):
+            max1.append(temp[1])
+        else:
+            dict1[temp[1]] = [temp[2],i]
+            max1.append(-1)
+    maxVal = max1[-1]  
+    for i in range(q - 2,-1,-1):
+        if(max1[i] > maxVal):
+            maxVal = max1[i]
+        elif(max1[i] < maxVal):
+            max1[i] = maxVal
+            
+    for i in range(n):
+        if((i + 1) in dict1):
+            arr[i] = max(dict1[i + 1][0],max1[dict1[i + 1][1]])
+        else:
+            arr[i] = max(arr[i],max1[0])
+            
+    for x in arr:
+        print(x,end=" ")
+    print()
+# region fastio
+BUFSIZE = 8192
+class FastIO(IOBase):
+    newlines = 0
+    def __init__(self, file):
+        self._fd = file.fileno()
+        self.buffer = BytesIO()
+        self.writable = "x" in file.mode or "r" not in file.mode
+        self.write = self.buffer.write if self.writable else None
+    def read(self):
+        while True:
+            b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+            if not b:
+                break
+            ptr = self.buffer.tell()
+            self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+        self.newlines = 0
+        return self.buffer.read()
+    def readline(self):
+        while self.newlines == 0:
+            b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+            self.newlines = b.count(b"\n") + (not b)
+            ptr = self.buffer.tell()
+            self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+        self.newlines -= 1
+        return self.buffer.readline()
+    def flush(self):
+        if self.writable:
+            os.write(self._fd, self.buffer.getvalue())
+            self.buffer.truncate(0), self.buffer.seek(0)
+class IOWrapper(IOBase):
+    def __init__(self, file):
+        self.buffer = FastIO(file)
+        self.flush = self.buffer.flush
+        self.writable = self.buffer.writable
+        self.write = lambda s: self.buffer.write(s.encode("ascii"))
+        self.read = lambda: self.buffer.read().decode("ascii")
+        self.readline = lambda: self.buffer.readline().decode("ascii")
+def print(*args, **kwargs):
+    """Prints the values to a stream, or to sys.stdout by default."""
+    sep, file = kwargs.pop("sep", " "), kwargs.pop("file", sys.stdout)
+    at_start = True
+    for x in args:
+        if not at_start:
+            file.write(sep)
+        file.write(str(x))
+        at_start = False
+    file.write(kwargs.pop("end", "\n"))
+    if kwargs.pop("flush", False):
+        file.flush()
+if sys.version_info[0] < 3:
+    sys.stdin, sys.stdout = FastIO(sys.stdin), FastIO(sys.stdout)
+else:
+    sys.stdin, sys.stdout = IOWrapper(sys.stdin), IOWrapper(sys.stdout)
+input = lambda: sys.stdin.readline().rstrip("\r\n")
+# endregion
+if __name__ == "__main__":
+    main()
